29e292d73cb8102aba0bf5540d8c2d8c7ebaf072
Rohit Tewari <rtewari056@gmail.com>
2022-07-18 22:25:22 +0530

    Removed React Router from the React App as GitHub Pages doesn’t support routers

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
@@ -8,9 +7,9 @@ export default function Navbar(props) {
       className={`navbar navbar-expand-lg navbar-dark bg-${props.themeMode}`}
     >
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+        <a className="navbar-brand" href="#">
           {props.title}
-        </Link>
+        </a>
         <button
           className="navbar-toggler"
           type="button"
@@ -28,15 +27,15 @@ export default function Navbar(props) {
         >
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
+              <a className="nav-link active" aria-current="page" href="#">
                 Home
-              </Link>
+              </a>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
+            {/* <li className="nav-item">
+              <a className="nav-link" href="/about">
                 {props.aboutText}
-              </Link>
-            </li>
+              </a>
+            </li> */}
           </ul>
 
           <div className="form-check form-switch form-check-reverse text-light">
